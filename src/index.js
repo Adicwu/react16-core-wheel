f14ac6d9d421ab38a7ad52dd5c877d8bda1265cf
adicwu <youzaiwu@qq.com>
2022-09-20 17:55:10 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,28 +1,40 @@
 import React from './core/react'
 import ReactDom from './core/react-dom'
 
-const style = {
-  padding: '8px',
-  border: '2px solid crimson'
+class Test extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      num: 0
+    }
+  }
+  clc = () => {
+    // console.log('123');
+    this.setState((e) => ({
+      num: e.num + 1
+    }))
+  }
+  render() {
+    // console.log(this);
+    return (
+      <div id="app">
+        {/* <h1>{this.state.num}</h1> */}
+        <button onClick={this.clc}>click</button>
+      </div>
+    )
+  }
 }
 
-const el = (
-  <div id="a1" style={style}>
-    a1
-    <div id="b1" style={style}>
-      b1
-      <div id="c1" style={style}>
-        c1
-      </div>
-      <div id="c2" style={style}>
-        c2
-      </div>
-    </div>
-    <div id="b2" style={style}>b2</div>
-  </div>
-)
-console.log(el);
+
+
+function Todo() {
+  return <h1>todo</h1>
+}
+
 ReactDom.render(
-  el,
+  (
+    <div id="test"><Todo /></div>
+  ),
+  // <h1 id="a1" />,
   document.getElementById('root')
 )
